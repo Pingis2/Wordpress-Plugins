be8fa1360966ba842c103a777a2dedf35ea4fb90
Pingis2 <anton@worksit.se>
2025-05-23 15:41:32 +0200

    updates

--- wp-content/plugins/amazing-plugins/build/tab-block/index.asset.php
+++ wp-content/plugins/amazing-plugins/build/tab-block/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '37da81034f9e9c52005d');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '105c84faefbc750f9055');
